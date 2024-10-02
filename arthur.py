84511fb61ada4956c97fe3921f2a961ba5fbe1df
cabelo2b <00001124171939sp@al.educacao.sp.gov.br>
2024-10-02 20:41:26 -0300

    Update arthur.py

---
+++ arthur.py
@@ -0,0 +1,33 @@
+numero_do_candidato = int(imput('digite o numero do candidato:')) 
+candidato1 = {
+    '50': nome do candidato = boulos 
+    '13': partido: partido dos trabalhsdores
+    '123': 'nome do vice: marta'
+    '13013': 'cargos ja ocupados: prefeito e vice-prefeita 
+    '113': 'idade: 35 e 37' 
+}
+print(candidato1.get('5
+print(candidato1.get('123'))
+print(candidato1.get('13013'))
+print(candidato1.get('113'))
+
+
+numero_do_candidato = ido nunes 
+    '8': partido: movimento democratico brasileiro
+    '987': 'mello araujo'
+    '4728': 'cargos
+print(candidato1.get('8')
+numero_do_candidato = int(imput('digite o numero do candidato:')) 
+candidato1
+
+numero_do_candidatoes 
+    '8': partido: movimento democratico brasileiro
+    '987': 'mello araujo'
+    '4728': 'cargos ja ocupados: prefeito e vice-prefeito
+    '971': 'idade: 46 e  57' 
+}
+print(candidato1.get('15'))
+print(candidato1.get('8'))
+print(candidato1.get('987'))
+print(candidato1.get('4728'))
+print(candidato1.get('971'))
